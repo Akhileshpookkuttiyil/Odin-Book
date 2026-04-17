95e1ae00242d84a96f6ff8986026ac756bb27366
Akhilesh <akhileshpookkuttiyil@gmail.com>
2026-04-17 12:23:05 +0000

    Add Comments feature

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,35 @@
+class CommentsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_post
+  
+  def create
+    @comment = @post.comments.build(comment_params)
+    @comment.user = current_user
+    if @comment.save
+      redirect_to @post, notice: "Comment added!"
+    else
+      redirect_to @post, alert: "Comment can't be blank."
+    end
+  end
+
+  def destroy
+    @comment = @post.comments.find(params[:id])
+    if @comment.user == current_user
+      @comment.destroy
+      redirect_to @post, notice: "Comment deleted."
+    else
+      redirect_to @post, alert: "You can't delete this comment."
+    end
+  end
+
+  private
+
+    def set_post
+    @post = Post.find(params[:post_id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
+
+end
