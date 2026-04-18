5fe1a521457fd5262b367f935bd1bdb332a74cbe
Akhilesh <akhileshpookkuttiyil@gmail.com>
2026-04-18 06:29:40 +0000

    Add Likes feature

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,25 @@
+class LikesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_post  
+  
+  def create
+    @like = @post.likes.build(user: current_user)
+    if @like.save
+      redirect_to @post, notice: "Post liked!"
+    else
+      redirect_to @post, alert: "Already liked this post."
+    end
+  end
+
+  def destroy
+    @like = @post.likes.find_by(user: current_user)
+    @like&.destroy
+    redirect_to @post, notice: "Post unliked."
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:post_id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :users, only: [:show]
   resources :posts, only: [:index, :new, :create, :show, :destroy] do
     resources :comments, only: [:create, :destroy]
+    resource :like, only: [:create, :destroy]
   end
   get "up" => "rails/health#show", as: :rails_health_check
   root "home#index"
