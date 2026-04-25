675ccf716185bf2199a19dc735d7cc36f621dafe
Akhilesh <akhileshpookkuttiyil@gmail.com>
2026-04-25 16:47:58 +0000

    Improve UI: smart_time helper, comment styles, avatar initials, accept/reject buttons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+    def smart_time(datetime)
+        if datetime > 7.days.ago
+            time_ago_in_words(datetime) + " ago"
+        else
+            datetime.strftime("%B %d, %Y")
+        end
+    end
 end
